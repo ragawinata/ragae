d1c0210d39fe30e257541d68715d2242ffa3adb5
ragawinata <pengabdiane@gmail.com>
2024-07-09 22:50:45 +0800

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // menghapus semua variabel sesi
-session_destroy(); // menghapus sesi
-
-header("Location: index.php"); // arahkan kembali ke halaman login atau halaman lainnya
-exit();
-?>
